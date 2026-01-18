2709f09e9129e1fd91d1f00a82502b1af3d2a1ee
EnzyI <zyxrywsp@gmail.com>
2026-01-18 22:49:59 +0700

    Update Docker connection messages in main.go

--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 func main() {
 	_, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
-		fmt.Println("Loi ket noi Docker")
+		fmt.Println("Loi: Khong ket noi duoc Docker")
 		return
 	}
 	fmt.Println("--- AETHER PANEL ---")
-	fmt.Println("Ket noi Docker thanh cong!")
+	fmt.Println("THANH CONG: Da ket noi voi Docker Engine!")
 }

cb96dd470e69bcbb328935ceecb0e2e038c4fe2f
EnzyI <zyxrywsp@gmail.com>
2026-01-18 23:00:35 +0700

    Set up Docker client and display OS information
    Initialize Docker client and retrieve system info.

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/client"
+)
 
 func main() {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		fmt.Println("Loi khoi tao: ", err)
+		return
+	}
+	
+	info, err := cli.Info(context.Background())
+	if err != nil {
+		fmt.Println("Loi ket noi Docker (Co the thieu quyen sudo): ", err)
+		return
+	}
+
 	fmt.Println("--- AETHER PANEL ---")
-	fmt.Println("HE THONG DA SAN SANG!")
+	fmt.Printf("THANH CONG! Docker OS: %s\n", info.OperatingSystem)
 }
